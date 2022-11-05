426d2bbd281002254f2798daadfd7bdb10598a31
segq3 <segq3.k@gmail.com>
2022-11-05 22:17:02 +0100

    new lesson added

---
+++ cas8/file.py
@@ -0,0 +1,48 @@
+# bodovi = int(input("unesite broj bodova 1-100: "))
+# if bodovi<0 or bodovi>100:
+#     print("broj bodova nije u opsegu")
+# else:
+#     if bodovi <= 50:
+#         ocena = "nedovoljan"
+#     elif bodovi <= 63:
+#         ocena = "dovoljan"
+#     elif bodovi <= 76:
+#         ocena = "dobar"
+#     elif bodovi <= 89:
+#         ocena = "vrlodobar"
+#     else:
+#         ocena = "odlican"
+#     print(ocena)
+
+# broj = int(input("unesite neki broj: "))
+# if broj % 3 == 0:
+#     print("plava")
+# elif broj % 3 == 1:
+#     print("crvena")
+# else:
+#     print("zelena")
+
+# for i in range(3):
+#     print("python")
+# for i in range(1,8):
+#     print(i)
+# for i in range(1,8,2):
+#     print(i)
+# for i in range(1,8,8):
+#     print(i)
+
+# for i in range(1,11):
+#     if i % 2 == 0:
+#         print("broj",i,"je paran")
+#     else:
+#         print("broj",i,"je neparan")
+
+# for i in range(1,100):
+#     if i % 2 == 0:
+#         print(i)
+
+# for i in range(1,11):
+#     print("unosimo policu broj", i)
+
+for i in range(6,0,-1):
+    print(i)
