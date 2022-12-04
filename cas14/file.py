9b9cc3f17a581b0e292a236f669bb13acc718329
segq3 <segq3.k@gmail.com>
2022-12-04 11:58:01 +0100

    new lesson added

---
+++ cas14/file.py
@@ -0,0 +1,25 @@
+print("""
+      program digitron
+      unesite broj od 1 do 5
+      1 - sabiranje
+      2 - oduzimanje
+      3 - mnozenje
+      4 - deljenje
+      5 - izlazak iz programa
+      """)
+
+while True:
+    operacija = int(input("odaberite operaciju (1,2,3,4,5): "))
+    if operacija == 5:
+        break
+    broj1 = int(input("unesite prvi broj: "))
+    broj2 = int(input("unesite drugi broj: "))
+    if operacija == 1:
+        print("zbir tih brojeva je:",broj1+broj2)
+    elif operacija == 2:
+        print("razlika tih brojeva je:",broj1-broj2)
+    elif operacija == 3:
+        print("proizvod tih brojeva je:",broj1*broj2)
+    elif operacija == 4:
+        print(" tih brojeva je:",broj1/broj2)
+    print()
