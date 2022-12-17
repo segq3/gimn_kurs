29a7d8124a8b5027c59f3d2f579fa6f5bd722839
segq3 <segq3.k@gmail.com>
2022-12-17 12:33:05 +0100

    lesson changed

--- cas17/file.py
+++ cas17/file.py
@@ -9,9 +9,15 @@
 #     print(y, end=' ')
 # print()
 
-broj = 12345
-niz = str(broj)
-niz2 = ''
-for i in range(len(niz)-1, -1, -1):
-    niz2 += niz[i]
-print(niz2)
+# broj = 12345
+# niz = str(broj)
+# niz2 = ''
+# for i in range(len(niz)-1, -1, -1):
+#     niz2 += niz[i]
+# print(niz2)
+
+lista = [10,20,30,40,50,60,70,80,90,100]
+
+for i in range(1,len(lista), 2):
+    print(lista[i], end=' ')
+print()
