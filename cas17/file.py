2a6abd80d646cfb386096e177632dec70488e6e0
segq3 <segq3.k@gmail.com>
2022-12-17 12:19:25 +0100

    lesson added

---
+++ cas17/file.py
@@ -0,0 +1,17 @@
+# x = 0
+# y = 1
+# print(x, end=' ')
+# print(y, end=' ')
+# for i in range(6):
+#     x += y
+#     print(x, end=' ')
+#     y += x
+#     print(y, end=' ')
+# print()
+
+broj = 12345
+niz = str(broj)
+niz2 = ''
+for i in range(len(niz)-1, -1, -1):
+    niz2 += niz[i]
+print(niz2)
